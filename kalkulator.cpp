1a24f04f1a57ba757f2043ac138b863339b1e4a4
ZxCraxss <87278112+ZyxMapLe@users.noreply.github.com>
2024-12-05 10:31:07 +0700

    Update kalkulator.cpp

--- kalkulator.cpp
+++ kalkulator.cpp
@@ -1,6 +1,66 @@
 #include <iostream>
-using namespace std;
-int main(){
+#include <cmath>
+#include<windows.h>
 
-return 0;
+using namespace std;
+
+
+void tampilan_kalkulator() {
+    cout<<"-----------------------------------------"<<endl;
+    cout<<"----                                  ---"<<endl;
+    cout<<"-             Kalkulator               -"<<endl;
+    cout<<"-             Matematika               -"<<endl;
+    cout<<"---                                   ---"<<endl;
+    cout<<"-----------------------------------------"<<endl;
+    cout<<"-      +     |      -      |      *     -"<<endl;
+    cout<<"-  Tambah    |   Kurang    |     Kali   -"<<endl;
+    cout<<"-----------------------------------------"<<endl;
+    cout<<"-      /     |      m      |      L     -"<<endl;
+    cout<<"-   Bagi     |     Mod     |     Log    -"<<endl;
+    cout<<"-----------------------------------------"<<endl;
+    cout<<"-      S     |      K      |      p     -"<<endl;
+    cout<<"-   Kuadrat  |    Kubik    |   Pangkat  -"<<endl;
+    cout<<"-----------------------------------------"<<endl;
+    cout<<"-      s     |      c      |      t     -"<<endl;
+    cout<<"-     sin    |     cos     |     tan    -"<<endl;
+    cout<<"-----------------------------------------"<<endl;
+    cout<<"-      r     |      C      |      .     -"<<endl;
+    cout<<"-    Akar    |    clear    |    close   -"<<endl;
+    cout<<"-----------------------------------------"<<endl;
+}
+
+
+void tampilan_output(double hasil) {
+    cout<<"-----------------------------------------"<<endl;
+    cout<<"----                     ----------------"<<endl;
+    cout<<"-     HASIL             |     "<<endl;
+    cout<<"-                       |  "<<hasil<<endl;
+    cout<<"---                      ----------------"<<endl;
+    cout<<"-----------------------------------------"<<endl;
+    cout<<"-      +     |      -      |      *     -"<<endl;
+    cout<<"-  Tambah    |   Kurang    |     Kali   -"<<endl;
+    cout<<"-----------------------------------------"<<endl;
+    cout<<"-      /     |      m      |      L     -"<<endl;
+    cout<<"-   Bagi     |     Mod     |     Log    -"<<endl;
+    cout<<"-----------------------------------------"<<endl;
+    cout<<"-      S     |      K      |      p     -"<<endl;
+    cout<<"-   Kuadrat  |    Kubik    |   Pangkat  -"<<endl;
+    cout<<"-----------------------------------------"<<endl;
+    cout<<"-      s     |      c      |      t     -"<<endl;
+    cout<<"-     sin    |     cos     |     tan    -"<<endl;
+    cout<<"-----------------------------------------"<<endl;
+    cout<<"-      r     |      C      |      .     -"<<endl;
+    cout<<"-    Akar    |    clear    |    close   -"<<endl;
+    cout<<"-----------------------------------------"<<endl;
+}
+
+int main() {
+    double angka1 = 0.0;
+    double angka2 = 0.0;
+
+    double hasil = 0.0;
+    char input = '\n';
+    bool konversi = 0;
+  
+    return 0;
 }
